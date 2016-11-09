b47a522715ad00e7fe69bb1e85a04116ed71c451
meelph <ThousandPies@web.de>
2016-11-09 15:04:05 +0100

    work on activity

--- app/app/src/main/java/de/ovgu/softwareprojekt/DiscoveryActivity.java
+++ app/app/src/main/java/de/ovgu/softwareprojekt/DiscoveryActivity.java
@@ -6,9 +6,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class DiscoveryActivity extends AppCompatActivity {
 
+    Button startDiscovery;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,14 +18,14 @@ public class DiscoveryActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        startDiscovery = (Button) findViewById(R.id.startDiscovery);
+        startDiscovery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startDiscovery();
             }
         });
+
     }
 
 }
