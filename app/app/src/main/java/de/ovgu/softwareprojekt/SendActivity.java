6dcb594372a87a6c4734809944bb94c19034dedc
meelph <ThousandPies@web.de>
2016-11-09 17:39:18 +0100

    ide shit changes

--- app/app/src/main/java/de/ovgu/softwareprojekt/SendActivity.java
+++ app/app/src/main/java/de/ovgu/softwareprojekt/SendActivity.java
@@ -9,5 +9,7 @@ public class SendActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send);
+
+
     }
 }
