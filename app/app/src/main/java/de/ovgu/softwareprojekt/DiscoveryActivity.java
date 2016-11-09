e112423fe68ce06fc99849f8a2c518df2e670f32
meelph <ThousandPies@web.de>
2016-11-09 17:11:14 +0100

    work in DiscoveryAct send to SendAct

--- app/app/src/main/java/de/ovgu/softwareprojekt/DiscoveryActivity.java
+++ app/app/src/main/java/de/ovgu/softwareprojekt/DiscoveryActivity.java
@@ -1,16 +1,25 @@
 package de.ovgu.softwareprojekt;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
-public class DiscoveryActivity extends AppCompatActivity {
+import java.util.List;
 
-    Button startDiscovery;
+import de.ovgu.softwareprojekt.server_discovery.Discovery;
+
+public class DiscoveryActivity extends AppCompatActivity implements Discovery.OnDiscoveryListener {
+
+    Button mStartDiscovery;
+    ListView mPossibleConnections;
+    List<Discovery.Server> mServerList;
+    Discovery mDiscovery;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +27,48 @@ public class DiscoveryActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        startDiscovery = (Button) findViewById(R.id.startDiscovery);
-        startDiscovery.setOnClickListener(new View.OnClickListener() {
+
+
+        mStartDiscovery = (Button) findViewById(R.id.startDiscovery);
+        mStartDiscovery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startDiscovery();
+                mDiscovery.startDiscovery(DiscoveryActivity.this);
+            }
+        });
+
+        mPossibleConnections = (ListView) findViewById(R.id.possibleConnections);
+
+        mPossibleConnections.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(DiscoveryActivity.this, SendActivity.class);
+                intent.putExtra("Name", mServerList.get(i).name);
+                intent.putExtra("Address" , mServerList.get(i).address);
+                intent.putExtra("Port", mServerList.get(i).port);
+
+                DiscoveryActivity.this.startActivity(intent);
             }
         });
 
     }
+
+    @Override
+    public void onServerListUpdated(List<Discovery.Server> serverNames) {
+
+        mServerList = serverNames;             //save server names and ips for further use
+
+        String[] stringNames = new String[serverNames.size()];
+
+
+        for(int i = 0 ; i < serverNames.size(); i++){
+            stringNames[i] = serverNames.get(i).name;
+
+        }
+
+        ArrayAdapter<String> serverNameAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, stringNames);
+        mPossibleConnections.setAdapter(serverNameAdapter);
+    }
+
 
 }
